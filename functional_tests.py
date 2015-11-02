c7182b461c1bbf3165c368addb331de8c86fb356
Josh <joshschertz3@gmail.com>
2015-11-02 01:38:56 -0600

    Implement Unittest and add first FT method to unittest class.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_list_and_retrieve_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finished the test')
+
+if __name__ == '__main__':
+
+    unittest.main()
