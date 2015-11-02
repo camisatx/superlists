b15031dff558bc12085888005c8867364e8b555c
Josh <joshschertz3@gmail.com>
2015-11-02 17:51:05 -0600

    Functional test now checks that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -12,9 +13,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_start_list_and_retrieve_later(self):
+
+        # Opens the homepage
         self.browser.get('http://localhost:8000')
 
+        # The page title and header mention 'To-Do' list
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
+
+        # Type 'Buy peacock feathers' into text box (for fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # Hit enter, the page updates and shows '1: Buy peacock feathers' as an
+        #   item in a to-do list table
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row
+                            in rows))
+
         self.fail('Finished the test')
 
 if __name__ == '__main__':
