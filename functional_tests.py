76856a7c2c9dd0498cd5e4c63f03e135bbf78f74
Josh <joshschertz3@gmail.com>
2015-11-03 00:14:28 -0600

    Expand app and FT to handle items in the list table

--- functional_tests.py
+++ functional_tests.py
@@ -34,11 +34,22 @@ class NewVisitorTest(unittest.TestCase):
         #   item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
+        # Page updates, showing that one item in the list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row
-                            in rows),
-                        'New to-do item did not appear in table')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # Add another item to the list: 'Use peacock feathers to make a fly'
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Page updates, showing both items in the list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
 
         self.fail('Finished the test')
 
