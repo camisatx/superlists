8684d264089b16a23534eb358786a6263deab37b
Josh <joshschertz3@gmail.com>
2015-11-03 23:00:41 -0600

    Refactor urls.py, including seperating lists urls into the lists app folder

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 # from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
     # url(r'^admin/', include(admin.site.urls)),
 ]
