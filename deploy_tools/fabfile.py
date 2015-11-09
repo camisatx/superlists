4d8421e48725149a531d4cdd3a69b625567e0fb3
Josh <joshschertz3@gmail.com>
2015-11-09 15:32:20 -0600

    Fix bugs in fabfile. Fab deploy now works for new and existing apps

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -38,9 +38,10 @@ def _get_latest_source(source_folder):
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, 'DEBUG = True', 'DEBUG = False')     # str substitution
-    sed(settings_path, 'ALLOWED_HOST =.+$', 'ALLOWED_HOST = ["%s"]' % site_name)
+    sed(settings_path, 'ALLOWED_HOSTS =.+$',
+        'ALLOWED_HOSTS = ["%s"]' % site_name)
 
-    secret_key_file = source_folder + '/superlists/secret_key.pmy'
+    secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
